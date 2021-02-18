c56e8bdf60fe82d559ed534c1f9ee42dc843a2d4
jrslagle <jrslagle@gmail.com>
2021-02-17 23:55:38 -0700

    trying to get Heroku to load that english model

--- my_module/nlp_model.py
+++ my_module/nlp_model.py
@@ -1,9 +1,11 @@
-import spacy
+# import spacy
+import en_core_web_sm
 
 class NLP():
     def __init__(self):
         # Load English pipeline
-        self.nlp = spacy.load('en_core_web_sm')  # small model, 2.6 s load time (output vector.shape = (96,))
+        # self.nlp = spacy.load('en_core_web_sm')
+        self.nlp = en_core_web_sm.load()  # small model, 2.6 s load time (output vector.shape = (96,))
         # self.nlp = spacy.load('en_core_web_trf')  # big model, 3.9 s load time, vector doesn't work
         # https://spacy.io/models
 
